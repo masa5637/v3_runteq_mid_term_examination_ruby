40a66c0709093776a0f7409eb8a2416b81e0cbd0
masa <masada59d@gmail.com>
2025-08-13 23:02:03 +0900

    課題提出

--- how_many_cats.rb
+++ how_many_cats.rb
@@ -1,3 +1,30 @@
 # ARGV[i]で引数の経過年数を取得できます。
 # 実行サンプル
+def cat_population(years)
+  kittens = 1
+  cats = 0
+  history = []
+
+  years.times do |year|
+    if year >= 2
+      new_cats = history[year - 2][:kittens]
+      cats += new_cats
+    end
+
+    new_kittens = cats
+    kittens += new_kittens
+  
+    history << {kittens: kittens, cats:cats}
+  end
+  { kittens: kittens, cats: cats }
+end
+
+
+
+
+
+
+
+
+
 puts ARGV[0]
